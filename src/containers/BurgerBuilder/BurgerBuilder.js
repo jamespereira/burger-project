6cd3548654cbc7ba7d95f7ac0e858d968cd59f24
James Pereira <james_pereira@apple.com>
2019-02-15 16:32:02 +1100

    burger project starter c150

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+
+export class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Fragment>
+    )
+  }
+}
+
+export default BurgerBuilder
