099066392076cc7997a5281bdbe5356c3d909639
James Pereira <james_pereira@apple.com>
2019-02-22 11:49:30 +1100

    update modal c162

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,13 +16,14 @@ const buildControls = (props) => {
     <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
       {controls.map(ctrl => (
         <BuildControl 
-        key={ctrl.label}  
-        label={ctrl.label} 
-        removed={() => props.ingredientRemoved(ctrl.type)}
-        added={() => props.ingredientAdded(ctrl.type)}
-        disabled={props.disabled[ctrl.type]}
+          key={ctrl.label}  
+          label={ctrl.label} 
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          added={() => props.ingredientAdded(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
         />
       ))}
+      <button className="OrderButton" disabled={!props.purchasable}>Order Now</button>
     </div>
   )
 }
